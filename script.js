be5138da7e1ccebbaa7ce5581951f2224d870506
gustavosouto02 <gustavo.pereira@a.ucb.br>
2024-04-18 08:30:20 -0300

    js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function colocarNaTela(dados) {
   document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name
   document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + '°C'
   document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+  document.querySelector('.umidade').innerHTML = 'Umidade: ' + dados.main.humidity + "%" 
 }
 
 async function buscarCidade(cidade) {
